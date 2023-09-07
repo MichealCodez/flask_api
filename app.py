f48cc36090f25638cfb146ae6585c3747ccae704
MichealDavid1 <madtechnow@gmail.com>
2023-09-07 14:46:13 +0100

    prepared for deployment

--- app.py
+++ app.py
@@ -16,15 +16,15 @@ def get_info():
     current_day_of_week = datetime.datetime.now(pytz.utc).strftime("%A")
     current_utc_time = datetime.datetime.now(pytz.utc)
 
-    utc_time_check = abs(current_utc_time.second) <= 120
+    # utc_time_check = abs(current_utc_time.second) <= 120
 
     response_data = {
         "slack_name": slack_name,
         "current_day": current_day_of_week,
         "utc_time": current_utc_time.strftime("%Y-%m-%dT%H:%M:%SZ"),
         "track": track,
-        "github_file_url": "https://github.com/username/repo/blob/main/file_name.ext",
-        "github_repo_url": "https://github.com/username/repo",
+        "github_file_url": "https://github.com/MichealDavid1/Task1_HNGX/blob/main/app.py",
+        "github_repo_url": "https://github.com/MichealDavid1/Task1_HNGX.git",
         "status_code": 200
     }
 
